868618d61ccc2d7a70d3aad1be812e6e8bf1a2dd
fayfan <sbaltz98@gmail.com>
2025-02-01 04:05:25 -0600

    Fifth commit

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,5 +3,10 @@ import type { Config } from '@react-router/dev/config';
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: false,
+  // ssr: false,
+
+  // Return a list of URLs to prerender at build time
+  async prerender() {
+    return ['/', '/projects', 'resume', 'contact'];
+  },
 } satisfies Config;
